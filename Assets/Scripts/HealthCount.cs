6da2b11d1b0aff2bc4327a67930b857b1148ae20
FearlessXD <2000zhenxuan@gmail.com>
2026-04-20 15:17:30 +0800

    Healthcount start

---
+++ Assets/Scripts/HealthCount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class HealthCount : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
